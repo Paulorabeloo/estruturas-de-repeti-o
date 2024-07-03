7a4676896f60a9d53d97dfcce27b34bf93bf496c
Paulorabeloo <pauloeduardorabelomiranda@gmail.com>
2024-07-03 10:52:39 -0300

    estruturas-de-repeticao

---
+++ src/ExemploBreakContinue.java
@@ -0,0 +1,36 @@
+public class ExemploBreakContinue 
+{
+	public static void main(String[] args) 
+	{
+	
+		for(int numero = 1; numero <=5; numero ++) // <= 5
+		{
+			if(numero==3)
+				break;								// VAI PARAR NO 3
+		
+			System.out.println(numero);
+		
+		}
+
+    }
+}
+
+
+/*public class ExemploBreakContinue 
+{
+	public static void main(String[] args) 
+	{
+	
+		for(int numero = 1; numero <=5; numero ++)
+		{
+			if(numero==3)                       
+				continue;							// VAI CONTINUAR MAS VAI PULAR O 3
+		
+			System.out.println(numero);
+		
+		}
+
+    }
+}
+
+*/
